9a1351cbfba78f862395adb55a64d4922168760d
Javier Botey <63394913+JaviBotey@users.noreply.github.com>
2023-03-21 14:30:56 +0100

    Update assoc.sh

--- Genotype/assoc.sh
+++ Genotype/assoc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #Test for the firth batch
-plink2 --pfile /vol/projects/CIIM/Lyme_GWAS/GWAS/Genetics/output/batch1/PostImp/merge_filtered_rsid --pheno iid-only /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/covars/pheno.tsv --covar iid-only /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/covars/covariates.tsv --covar-name sex --glm --ci 0.95 --out /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_500FG
+plink2 --pfile output/batch1/PostImp/merge_filtered_rsid --pheno iid-only covars/pheno.tsv --covar iid-only covars/covariates.tsv --covar-name sex --glm --ci 0.95 --out out/lyme1_500FG
 
 #Test for the second batch
-plink2 --pfile /vol/projects/CIIM/Lyme_GWAS/GWAS/Genetics/output/batch2/PostImp/merge_filtered_rsid --pheno iid-only /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/covars/pheno.tsv --covar iid-only /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/covars/covariates.tsv --covar-name sex --glm --ci 0.95 --out /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme2_300BCG
+plink2 --pfile output/batch2/PostImp/merge_filtered_rsid --pheno iid-only /covars/pheno.tsv --covar iid-only covars/covariates.tsv --covar-name sex --glm --ci 0.95 --out out/lyme2_300BCG
 
 #Metal for both
 metal metal.sh
-mv METAANALYSIS1.TBL.info /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_lyme2.metal.tbl.info
-mv METAANALYSIS1.TBL /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_lyme2.metal.tbl
+mv METAANALYSIS1.TBL.info out/lyme1_lyme2.metal.tbl.info
+mv METAANALYSIS1.TBL out/lyme1_lyme2.metal.tbl
 
 #Metal for both and finngen
 #First, we need to cut only the rsid
-cat  /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_500FG.pheno.glm.logistic.hybrid | sed 's/ID/%ID/g'  | cut -f 2 -d % > /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_500FG_rsid.pheno.glm.logistic.hybrid
+cat  out/lyme1_500FG.pheno.glm.logistic.hybrid | sed 's/ID/%ID/g'  | cut -f 2 -d % > out/lyme1_500FG_rsid.pheno.glm.logistic.hybrid
 
-cat  /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme2_300BCG.pheno.glm.logistic.hybrid | sed 's/ID/%ID/g'  | cut -f 2 -d % > /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme2_300BCG_rsid.pheno.glm.logistic.hybrid
+cat  out/lyme2_300BCG.pheno.glm.logistic.hybrid | sed 's/ID/%ID/g'  | cut -f 2 -d % > out/lyme2_300BCG_rsid.pheno.glm.logistic.hybrid
 
 metal metal_finngen.sh
-mv METAANALYSIS1.TBL.info /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_lyme2_finngen.metal.tbl.info
-mv METAANALYSIS1.TBL /vol/projects/CIIM/Lyme_GWAS/GWAS/assoc/out/lyme1_lyme2_finngen.metal.tbl
+mv METAANALYSIS1.TBL.info out/lyme1_lyme2_finngen.metal.tbl.info
+mv METAANALYSIS1.TBL out/lyme1_lyme2_finngen.metal.tbl
